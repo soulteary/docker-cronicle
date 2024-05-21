7c9d6510e6ba8dfa503a73e8e89b2e3fd2521235
Zachary Bedell <zac@thebedells.org>
2024-05-21 13:40:28 -0400

    Don't force main server if already configured - fixes loss of worker servers on restart and #11

--- docker/docker-entrypoint.js
+++ docker/docker-entrypoint.js
@@ -1,57 +1,62 @@
 #!/usr/bin/env node
 
-const { existsSync, unlinkSync } = require("fs");
-const { dirname } = require("path");
-const { exec } = require("child_process");
-const { hostname, networkInterfaces } = require("os");
-const StandaloneStorage = require("pixl-server-storage/standalone");
+if(require("fs").existsSync("./data/users")) {
+  console.log("Docker Env already configured.");
+  require("../lib/main.js");
+} else {
+  const { existsSync, unlinkSync } = require("fs");
+  const { dirname } = require("path");
+  const { exec } = require("child_process");
+  const { hostname, networkInterfaces } = require("os");
+  const StandaloneStorage = require("pixl-server-storage/standalone");
 
-if (existsSync("./logs/cronicled.pid")) unlinkSync("./logs/cronicled.pid");
+  if (existsSync("./logs/cronicled.pid")) unlinkSync("./logs/cronicled.pid");
 
-if (!existsSync("./data/users")) {
-  exec("/opt/cronicle/bin/control.sh setup", (error, stdout, stderr) => {
-    if (error || stderr) {
-      console.log("init strorage failed");
-      console.log(error.message || stderr);
-      process.exit(1);
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-}
+  if (!existsSync("./data/users")) {
+    exec("/opt/cronicle/bin/control.sh setup", (error, stdout, stderr) => {
+      console.log("Storage init.");
+      if (error || stderr) {
+        console.log("init strorage failed");
+        console.log(error.message || stderr);
+        process.exit(1);
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  }
 
-process.chdir(dirname(__dirname));
+  process.chdir(dirname(__dirname));
 
-const config = require("../conf/config.json");
-const storage = new StandaloneStorage(config.Storage, function (err) {
-  if (err) throw err;
+  const config = require("../conf/config.json");
+  const storage = new StandaloneStorage(config.Storage, function (err) {
+    if (err) throw err;
+    const dockerHostName = (process.env["HOSTNAME"] || process.env["HOST"] || hostname()).toLowerCase();
 
-  const dockerHostName = (process.env["HOSTNAME"] || process.env["HOST"] || hostname()).toLowerCase();
+    const networks = networkInterfaces();
+    const [ip] = Object.keys(networks)
+      .filter((eth) => networks[eth].filter((addr) => addr.internal === false && addr.family === "IPv4").length)
+      .map((eth) => networks[eth])[0];
 
-  const networks = networkInterfaces();
-  const [ip] = Object.keys(networks)
-    .filter((eth) => networks[eth].filter((addr) => addr.internal === false && addr.family === "IPv4").length)
-    .map((eth) => networks[eth])[0];
+    const data = {
+      type: "list_page",
+      items: [{ hostname: dockerHostName, ip: ip.address }],
+    };
 
-  const data = {
-    type: "list_page",
-    items: [{ hostname: dockerHostName, ip: ip.address }],
-  };
-
-  const key = "global/servers/0";
-  storage.put(key, data, function () {
-    storage.shutdown(function () {
-      console.log("Record successfully saved: " + key + "\n");
-      storage.get(key, function (_, data) {
-        if (storage.isBinaryKey(key)) {
-          console.log(data.toString() + "\n");
-        } else {
-          console.log((typeof data == "object" ? JSON.stringify(data, null, "\t") : data) + "\n");
-        }
-        storage.shutdown(function () {
-          console.log("Docker Env Fixed.");
-          require("../lib/main.js");
+    const key = "global/servers/0";
+    storage.put(key, data, function () {
+      storage.shutdown(function () {
+        console.log("Record successfully saved: " + key + "\n");
+        storage.get(key, function (_, data) {
+          if (storage.isBinaryKey(key)) {
+            console.log(data.toString() + "\n");
+          } else {
+            console.log((typeof data == "object" ? JSON.stringify(data, null, "\t") : data) + "\n");
+          }
+          storage.shutdown(function () {
+            console.log("Docker Env Fixed.");
+            require("../lib/main.js");
+          });
         });
       });
     });
   });
-});
+}
